f160178fc3d5e76ee51cd78b6dd71c96c05be4d9
vocaotangoc <caongoctanvo@gmail.com>
2025-09-30 21:27:25 +0700

    tuần 1: thực hiện ứng dụng UI

---
+++ week1/codew1/app/src/main/java/com/example/btw1/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.example.btw1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.btw1.ui.theme.Btw1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Btw1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(0xFFF1F1F1) // màu nền ngoài cùng
+                ) {
+                    ProfileCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileCard(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(0.dp),
+        shape = RoundedCornerShape(24.dp),
+        shadowElevation = 8.dp,
+        color = Color.White
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Hàng nút trên cùng
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(10.dp, 20.dp, 10.dp, 0.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(12.dp))
+                ) {
+                    IconButton(onClick = { /* TODO: Back */ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrowleft), // icon của bạn
+                            contentDescription = "Back",
+                            tint = Color.Black,       // chỉnh màu, ví dụ đen
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(12.dp))
+                ) {
+                    IconButton(
+                        onClick = { /* TODO: Back */ },
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.edit), // icon của bạn
+                            contentDescription = "Edit",
+                            tint = Color.Black,       // chỉnh màu, ví dụ đen
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(200.dp))
+
+            // Avatar
+            Image(
+                painter = painterResource(id = R.drawable.avatar), // thay avatar.png trong res/drawable
+                contentDescription = "Profile picture",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Tên
+            Text(
+                text = "Võ Cao Tấn Ngọc",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            // Địa chỉ
+            Text(
+                text = "Hồ Chí Minh, Việt Nam",
+                fontSize = 16.sp,
+                color = Color.Gray
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileCardPreview() {
+    Btw1Theme {
+        ProfileCard()
+    }
+}
